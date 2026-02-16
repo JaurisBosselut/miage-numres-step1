301ae69d488e3a2c030d446bde4a115962bef66f
JaurisBosselut <jauris.bosselut83@gmail.com>
2026-02-16 13:49:13 +0000

    Modif quarkus hello

--- api-quarkus/src/main/java/org/aepsilon/GreetingResource.java
+++ api-quarkus/src/main/java/org/aepsilon/GreetingResource.java
@@ -10,7 +10,16 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String sayHello() {
+        return "Je m'présente, je m'appelle Henri\n"
+            + "J'voudrais bien réussir ma vie, être aimé\n"
+            + "Être beau, gagner de l'argent\n"
+            + "Puis surtout être intelligent\n"
+            + "Mais pour tout ça il faudrait que j'bosse à plein temps\n\n"
+            + "J'suis chanteur, je chante pour mes copains\n"
+            + "J'veux faire des tubes et que ça tourne bien, tourne bien\n"
+            + "J'veux écrire une chanson dans le vent\n"
+            + "Un air gai, chic et entraînant\n"
+            + "Pour faire danser dans les soirées de Monsieur Durand";
     }
 }
