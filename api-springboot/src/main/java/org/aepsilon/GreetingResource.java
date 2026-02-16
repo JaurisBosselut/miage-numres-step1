f4355f5059a0e855786a1e2112caa26173fdb688
JaurisBosselut <jauris.bosselut83@gmail.com>
2026-02-16 13:30:15 +0000

    Modification message hello

--- api-springboot/src/main/java/org/aepsilon/GreetingResource.java
+++ api-springboot/src/main/java/org/aepsilon/GreetingResource.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class GreetingResource {
 
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Je m'présente, je m'appelle Henri\n"
+            + "J'voudrais bien réussir ma vie, être aimé\n"
+            + "Être beau, gagner de l'argent\n"
+            + "Puis surtout être intelligent\n"
+            + "Mais pour tout ça il faudrait que j'bosse à plein temps\n\n"
+            + "J'suis chanteur, je chante pour mes copains\n"
+            + "J'veux faire des tubes et que ça tourne bien, tourne bien\n"
+            + "J'veux écrire une chanson dans le vent\n"
+            + "Un air gai, chic et entraînant\n"
+            + "Pour faire danser dans les soirées de Monsieur Durand";
     }
 }
